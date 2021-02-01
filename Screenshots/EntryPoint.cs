490746a93c5cf1a9424d84890d85eb05f914242d
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-02-01 10:10:44 +0530

    Add project files.

---
+++ Screenshots/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace Screenshots
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver chrome = new ChromeDriver();
+
+            chrome.Navigate().GoToUrl("https://www.google.com/");
+
+            Screenshot googleScreenshot = ((ITakesScreenshot)chrome).GetScreenshot();
+
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\screenshots");
+            googleScreenshot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshots.png", ScreenshotImageFormat.Png);
+
+            chrome.Quit();
+        }
+    }
+}
